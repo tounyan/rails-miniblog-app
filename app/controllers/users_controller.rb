513fd36cdf2adf7b3bea307730287da7d889e013
Tounyan <dmthu92@gmail.com>
2021-08-12 22:41:21 +1000

    Complete user signup page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,8 @@ class UsersController < ApplicationController
    def create
       @user = User.new(user_params)
       if @user.save
-         #code
+         flash[:success] = "Welcome to the Miniblog App"
+         redirect_to @user
       else
          render 'new'
       end
