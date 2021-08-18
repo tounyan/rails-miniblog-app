f819ead9d14ed3e3ed32d6c4a3c93dd31a1722bf
Tounyan <dmthu92@gmail.com>
2021-08-18 22:57:53 +1000

    Allow user to log out and add appropriate tests

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
 
    def create
       user = User.find_by(email: params[:session][:email].downcase)
-      if user && user.authenticate(params[:session][:password])
+      if user&.authenticate(params[:session][:password])
          log_in user
          redirect_to user
       else
@@ -15,5 +15,7 @@ class SessionsController < ApplicationController
    end
 
    def destroy
+      log_out
+      redirect_to root_path
    end
 end
